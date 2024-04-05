46690709df87c6e7328968d2fd93e966b534043e
unknown <ngoctruong274@gmail.com>
2024-04-05 21:25:25 +0700

    first commit

---
+++ src/main/java/com/gnourt/taskify/TaskifyApplication.java
@@ -0,0 +1,13 @@
+package com.gnourt.taskify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gnourt/taskify/TaskifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnourt.taskify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
